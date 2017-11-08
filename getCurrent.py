e4c8817a6e52231cfd33ec9e8c00f8fa5c3f9567
Grey Christoforo <grey@christoforo.net>
2017-11-08 17:07:57 +0100

    Create getCurrent.py

---
+++ getCurrent.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# written by grey@christoforo.net
+# on 8 Nov 2017
+
+# prints current from keithley 24xx connected via serial
+
+import serial
+
+port = '/dev/ttyUSB0'
+baud = 9600
+timeout = 3 # seconds
+
+expectedDeviceString = b'KEITHLEY INSTRUMENTS INC.,MODEL 2410,4090615,C33   Mar 31 2015 09:32:39/A02  /J/K\r\n'
+
+k = serial.Serial(port,baud,timeout=timeout)
+
+k.write(b'*RST\n')
+k.write(b'*IDN?\n')
+deviceString = k.readline()
+
+if deviceString == expectedDeviceString:
+  pass
+  #print('Conected to', deviceString.decode("utf-8"))
+else:
+  print('ERROR: Got unexpected device string:', deviceString.decode("utf-8"))
+  exit()
+
+
+cmds = []
+
+cmds.append(':SOUR:FUNC VOLT')
+cmds.append(':SOUR:VOLT:MODE FIXED')
+cmds.append(':SENS:FUNC "CURR"')
+cmds.append(':SENS:CURR:NPLC 10')
+cmds.append(':SOUR:VOLT:RANG MIN')
+cmds.append(':SOUR:VOLT:LEV 0')
+cmds.append(':FORM:ELEM CURR')
+cmds.append(':OUTP ON')
+
+for cmd in cmds:
+  #print('Sending', cmd)
+  cmd = cmd.encode('utf-8') + b'\n'
+  k.write(cmd)
+  #k.write(b':SYST:ERR?\n')
+  #err = k.readline()
+  #print(err)
+
+cmd = ':READ?'
+k.write(cmd.encode('utf-8') + b'\n')
+current = float(k.readline().decode('utf-8'))
+
+print(current)
+
+cmd = ':OUTP OFF'
+k.write(cmd.encode('utf-8') + b'\n')
+#k.write(b':SYST:ERR?\n'); k.readline()
+
+k.close()
